6fe0f312b63f0d0a63e4fe59ceee1f0b2b59444e
Joel <joel.balmer@gmail.com>
2015-11-29 01:36:50 +0100

    testing vimeo api

--- js/grayscale.js
+++ js/grayscale.js
@@ -104,6 +104,43 @@ window.onPlay = function(id) {
 //YOUTUBE APIS END
 
 
+//VIMEO API
+$(function() {
+    var iframe = $('#macmillanCancerVideo')[0];
+    var player = $f(iframe);
+    var status = $('.status');
+
+    // When the player is ready, add listeners for pause, finish, and playProgress
+    player.addEvent('ready', function() {
+        status.text('ready');
+        
+        player.addEvent('pause', onPause);
+        player.addEvent('finish', onFinish);
+        player.addEvent('playProgress', onPlayProgress);
+    });
+
+    // Call the API when a button is pressed
+    $('button').bind('click', function() {
+        player.api($(this).text().toLowerCase());
+    });
+
+    function onPause(id) {
+        status.text('paused');
+        console.log('paused' + status.text);
+    }
+
+    function onFinish(id) {
+        status.text('finished');
+        console.log('finished' + status.text);
+    }
+
+    function onPlayProgress(data, id) {
+        status.text(data.seconds + 's played');
+    }
+});
+//VIMEO API END
+
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
